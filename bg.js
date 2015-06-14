d8c3d12a3f80ba7f3dc795c72e3bc2e48ef654ed
joeyutong <joeyutong@gmail.com>
2015-06-14 13:02:20 +0800

    delete code comment

--- bg.js
+++ bg.js
@@ -1,9 +1,3 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// A generic onclick callback function.
-
 function genericOnClick(info, tab) {
   chrome.tabs.sendMessage(tab.id, {message: "scrollTo"}, function (res) {
     console.log(res.result);
